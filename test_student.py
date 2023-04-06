8cc0ffa0b48645654ac1cdee295de46456f3eef8
Lucia Ferencik <lferencikova@gmail.com>
2023-04-06 10:34:15 +0000

    write unit tests for evens and student class

---
+++ test_student.py
@@ -0,0 +1,61 @@
+import unittest
+from student import Student
+from datetime import timedelta
+
+
+class TestStudent(unittest.TestCase):
+    """
+    * Create a new test method called test_apply_extension
+    * Inside test_apply_extension, store the current end_date for our student
+    instance in a variable called old_end_date
+    * Call a method named apply_extension that will take a number of days
+    as an argument on the student instance to update the end_date
+    * Assert whether the instance's end_date equals the old end date plus
+     the days argument that was passed in using timedelta
+    * Run the tests to confirm that the new method is failing
+    In the Student calls, create a new moethod called apply_extension that
+     has a parameter called days
+    Use the timedelta method from datetime to update the end_date property
+    * Run the tests to confrim they are working
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+    def tearDown(self):
+        print('tearDown')
+
+    def test_full_name(self):
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, 'John Doe')
+
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email_address(self):
+        print("test_email_address")
+        self.assertEqual(self.student.email_address, 'john.doe@email.com')
+
+    def test_apply_extension(self):
+        print("test_apply_extension")
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(
+            days=5))
+
+
+if __name__ == "__main__":
+    unittest.main()
